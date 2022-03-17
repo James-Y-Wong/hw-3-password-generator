4a534113c8e4ea1248ce272442f5d0158aa42fa9
James-Y-Wong <jamesyeuwong@gmail.com>
2022-03-17 18:05:58 -0400

    added global variables to script.js: still need to fix var special

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var special = [ , "!", "/#", "/$", "/%", "/&", "/'", "/(", "/)", "/*", "/+", "/,", "/-", "/.", "//", "/:", "/;", "/<", "/=", "/>", "/?", "/@", "/[", "/\", /], /^, /_, /', /{, /|, /}, /~"];
+
+
+
+console.log(special);
+console.log(uppercase);
+console.log(lowercase);
+console.log(numeric);
 
 // Write password to the #password input
 function writePassword() {
