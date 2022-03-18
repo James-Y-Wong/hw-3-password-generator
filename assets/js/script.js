0418dd8595fd61a1eef5de7f427067ae3e04c05d
James-Y-Wong <jamesyeuwong@gmail.com>
2022-03-17 22:25:35 -0400

    fixed var special and able to console log correct special characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,8 @@ var generateBtn = document.querySelector("#generate");
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var special = [" ", "!", "\"", #$%&'()*+,-./:;<=>?@[\]^_`{|}~];
+var special = [" ", "!", "\"", "#", "$", "%", "&", "\'", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 
-
-console.log(special);
-console.log(uppercase);
-console.log(lowercase);
-console.log(numeric);
 
 // Write password to the #password input
 function writePassword() {
@@ -22,3 +17,9 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var passGen = function() {
+  var lowerCaseOption = window.prompt("Would you like to include lowercase characters?");
+}
+
+passGen()
